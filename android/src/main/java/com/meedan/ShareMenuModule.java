9a76447f1445ecfd4d6a8b0a4c481dea6cda87e4
Denver Sessink <denver@dsinternet.nl>
2017-02-04 19:35:33 +0100

    Added support for sharing images

--- android/src/main/java/com/meedan/ShareMenuModule.java
+++ android/src/main/java/com/meedan/ShareMenuModule.java
@@ -13,6 +13,7 @@ import java.util.Map;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 
 public class ShareMenuModule extends ReactContextBaseJavaModule {
 
@@ -29,14 +30,30 @@ public class ShareMenuModule extends ReactContextBaseJavaModule {
   public void getSharedText(Callback successCallback) {
     Activity mActivity = getCurrentActivity();
     Intent intent = mActivity.getIntent();
-    String inputText = intent.getStringExtra(Intent.EXTRA_TEXT);
-    successCallback.invoke(inputText);
+    String action = intent.getAction();
+    String type = intent.getType();
+
+    if (Intent.ACTION_SEND.equals(action) && type != null) {
+      if ("text/plain".equals(type)) {
+        String input = intent.getStringExtra(Intent.EXTRA_TEXT);
+        successCallback.invoke(input);
+      }
+      else if (type.startsWith("image/")) {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        successCallback.invoke(imageUri.toString());
+      }
+    }
   }
 
   @ReactMethod
   public void clearSharedText() {
     Activity mActivity = getCurrentActivity();
     Intent intent = mActivity.getIntent();
-    intent.removeExtra(Intent.EXTRA_TEXT);
+    String type = intent.getType();
+    if ("text/plain".equals(type)) {
+      intent.removeExtra(Intent.EXTRA_TEXT);
+    } else if (type.startsWith("image/")) {
+      intent.removeExtra(Intent.EXTRA_STREAM);
+    }
   }
 }
